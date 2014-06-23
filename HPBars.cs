98e58c698a1f38260a4f70df30e26da07266efb0
krishnaglick <unstoppable411@gmail.com>
2014-06-23 18:40:54 -0400

    First

---
+++ HPBars.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HPBars : MonoBehaviour {
+
+	//The Gameobject to appear above
+	private GameObject target;
+
+	//Foreground color, aka the health color
+	private Texture2D Foreground;
+	//Background color, aka what you see when you've lost hp
+	private Texture2D Background;
+
+	//The mob's max hp
+	private float MaxHP;
+	//The mob's current hp
+	private float CurrentHP;
+
+	//This creates a new instance of my color class
+	private Textures Colors = new Textures();
+
+	public void updateHP(float HP, bool addHP)
+	{
+		//This is called in the attachee's control script
+		//HP is amount to add/subtract, addHP being true adds, otherwise subtracts
+		if(addHP)
+			CurrentHP += HP;
+		else
+			CurrentHP -= HP;
+
+		//Destroy the enemy if their hp drops below 0
+		if(CurrentHP <= 0)
+			Destroy(target);
+
+		//This controls hp bar colors
+		if(CurrentHP / MaxHP > .5)
+			Foreground = Colors.GetColor("green");
+		if (CurrentHP / MaxHP <= .5)
+			Foreground = Colors.GetColor("yellow");
+		if (CurrentHP / MaxHP <= .25)
+			Foreground = Colors.GetColor("orange");
+		if (CurrentHP / MaxHP <= .15)
+			Foreground = Colors.GetColor("red");
+	}
+
+	public void Init(GameObject go, float HP)
+	{
+		//Set max hp to passed in value
+		MaxHP = HP;
+		//Setting target to go to keep track of the attachee
+		target = go;
+		//Sets the current hp to max hp, at spawn the mob should be max hp
+		CurrentHP = MaxHP;
+		//Sets the default foreground and background colors
+		Foreground = Colors.GetColor("green");
+		Background = Colors.GetColor("black");
+	}
+
+	void OnGUI()
+	{
+		//This keeps track of the mob relative to the screen
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(target.transform.position);
+		//Gets the x position of the mob and centers the hp bar
+		float xPosition = screenPos.x - (MaxHP / 2);
+		//Gets the mob's y position relative to the screen with an offset that won't screw up on multiple screen sizes
+		float yPosition = Camera.main.pixelHeight - screenPos.y - (Camera.main.pixelHeight / 8);
+		//Scales the bar's height based on the size of the enemy
+		float barHeight = target.renderer.bounds.size.y * 6;
+		//Draws the hp and background bars
+		GUI.DrawTexture(new Rect(xPosition, yPosition, MaxHP, barHeight), Background);
+		GUI.DrawTexture(new Rect(xPosition, yPosition, CurrentHP, barHeight), Foreground);
+	}
+}
---
+++ Textures.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Textures
+{
+	private Dictionary<string, Texture2D> Colors = new Dictionary<string, Texture2D>();
+
+	public Textures()
+	{
+		//Need a 1px by 1px png or bmp w/ the matching name, located in Assets/Resources
+		Colors.Add("black", (Texture2D) Resources.Load("Texture_Black", typeof(Texture2D)));
+		Colors.Add("green", (Texture2D) Resources.Load("Texture_Green", typeof(Texture2D)));
+		Colors.Add("red", (Texture2D) Resources.Load("Texture_Red", typeof(Texture2D)));
+		Colors.Add("orange", (Texture2D) Resources.Load("Texture_Orange", typeof(Texture2D)));
+		Colors.Add("yellow", (Texture2D) Resources.Load("Texture_Yellow", typeof(Texture2D)));
+	}
+
+	public Texture2D GetColor(string color)
+	{
+		//Returns the color you're looking for
+		return Colors[color.ToLower()];
+	}
+}
